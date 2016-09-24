c0c3aa6393a062828b4bfaad913f48c7d3f0fb1a
baymax <baymaxhuang@gmail.com>
2016-09-24 21:15:24 +0800

    forICC

--- req_top_gen_hhb.c
+++ req_top_gen_hhb.c
@@ -14,13 +14,20 @@
 #define LEN_CMD 128
 #define SCALE 100
 #define MAX_STR_LEN 1024
+#define MAX_NODE_NUM 100
 
-#define POISSON_MEAN 5 //per 100 time
-//#define POISSON_MEAN 50 //per 1000 time
-#define TOTAL_TIME 10000 //for the 100 requests
+#define POISSON_MEAN 5 //per 100 time unit
+//#define POISSON_MEAN 50 //per 1000 time unit
+#define TOTAL_TIME 10000 //for the 500 requests
+#define EXP_MEAN 400
 
-#define MAX_BW 100
-#define MAX_CPU 100
+#define MAX_CPU 20
+#define MIN_CPU 5
+#define MAX_BW 15
+#define MIN_BW 5
+#define MAX_TABLE 6
+#define MIN_TABLE 2
+#define THETA 0.5
 
 int poisson(double lambda) {
   double p;
@@ -43,9 +50,9 @@ int main(int argc,char **argv)
     int xseed = (unsigned)time(NULL);  // Current Time
     srand(xseed); // seed
 
-    if (argc != 7)
+    if (argc != 6)
     {
-        printf("Wrong input parameters!\n Usage:\n top_gen <n> <link_r> <max_node_num> <min_node_num>  <location> <dir_name>\nn: the number of requests\nlink_r: the link connectivity rate\nmax_node_num: maximum node number\nmin_node_num: minimum node number\ndir_name: the directory the generated requests would be put in\n\n");
+        printf("Wrong input parameters!\n Usage:\n top_gen <n> <link_r> <max_node_num> <min_node_num> <dir_name>\nn: the number of requests\nlink_r: the link connectivity rate\nmax_node_num: maximum node number\nmin_node_num: minimum node number\ndir_name: the directory the generated requests would be put in\n\n");
         exit(0);
     }
 
@@ -54,16 +61,16 @@ int main(int argc,char **argv)
     double link_conn_rate = atof(argv[2]);// link connectivity rate
     int max_node_num = atoi(argv[3]);//maximum node number
     int min_node_num = atoi(argv[4]);//minimum node number
-    int location_to_file = atoi(argv[5]);
 
     FILE * fp_spec;
     FILE * fp_req;
     char specfilename[LEN_FILENAME], reqfilename[LEN_FILENAME];
     char cmd[LEN_CMD];
 
-    int i,j,node_num,num_nodes,num_edges,from,to,t1,t2;
+    int i,j,node_num,num_nodes,num_edges,t1,t2;
+	//k表示一个时隙内请求的个数，服从泊松分布
 	int k = 0,  countk = 0, p = 0, start, time, duration;
-    int *n_x,*n_y;
+    int *n_x, *n_y, *from, *to;
     int check_delete = system("rm -fr spec/itm*");
     if(check_delete == -1)
     {
@@ -94,7 +101,7 @@ int main(int argc,char **argv)
 		//  5: Exponential
 		//  6: Locality
 
-		fprintf(fp_spec,"%d %d 3 %.2lf \n",node_num,SCALE,link_conn_rate);  // scale: SCALE; method: 3; alpha: 0.5; beta: 0.2; gamma: default  3 - pure random
+		fprintf(fp_spec,"%d %d 3 %.2lf \n",node_num,SCALE,link_conn_rate);  // scale: SCALE; method: 3; alpha: 0.5; beta: 0.2; THETA: default  3 - pure random
 		printf("%d %d 3 %.2lf \n",node_num,SCALE,link_conn_rate);
 		fclose(fp_spec);
 	}
@@ -114,7 +121,7 @@ int main(int argc,char **argv)
 
 	char req_folder[LEN_FILENAME];
 
-	sprintf(req_folder,"./req_topos/%s",argv[6]);
+	sprintf(req_folder,"./req_topos/%s",argv[5]);
 	
 	if(access(req_folder,2) != 0)
 	{
@@ -143,7 +150,7 @@ int main(int argc,char **argv)
 			}
 			else
 			{
-				sprintf(req_folder,"./req_topos/new_%s",argv[6]);
+				sprintf(req_folder,"./req_topos/new_%s",argv[5]);
 				if(access(req_folder,2) != 0)
 				{
 					mkdir(req_folder,0777);
@@ -155,6 +162,7 @@ int main(int argc,char **argv)
 	}
 
 	char str[MAX_STR_LEN];
+	int degree[MAX_NODE_NUM];
 	for (i = 0; i < n; i ++)
 	{
 		sprintf(reqfilename,"%s/req%d.txt",req_folder, i);
@@ -189,7 +197,7 @@ int main(int argc,char **argv)
 		fscanf(fp_spec,"%d %d %*d %*d",&num_nodes,&num_edges);
 		num_edges /= 2;
 		
-		//请求的达到服从泊松分布，请求的持续时间服从指数分布
+		//新的时隙开始，请求的达到服从泊松分布，请求的持续时间服从指数分布
 		if (countk == k) {
 			k = 0;
 			while( k==0)
@@ -199,6 +207,7 @@ int main(int argc,char **argv)
 			start = (p * TOTAL_TIME*POISSON_MEAN)/n;
 			p ++; 
 		}
+		//在一个时隙内，k个请求按时间均匀分布
 		time = start + (countk+1)*TOTAL_TIME*POISSON_MEAN/(n*(k+1));
 		countk ++;
 		/*if (rand()/(double)MY_RAND_MAX < LONG_REQ_RATE) {
@@ -206,32 +215,28 @@ int main(int argc,char **argv)
 		} else {
 			duration = SHORT_REQ_DURATION;
 		}*/
-		duration = (int)(-log(rand()/(double)MY_RAND_MAX)*1000);
+		duration = (int)(-log(rand()/(double)MY_RAND_MAX)*EXP_MEAN);
 
-		fprintf(fp_req,"%d %d 0 %d %d\n",num_nodes,num_edges, time, duration);
+		fprintf(fp_req,"%d %d 0 %d %d 0 %f\n",num_nodes,num_edges, time, duration, THETA);
 		printf("time %d, duration %d\n", time, duration);
 
-		if (location_to_file == 1)
+		//skip
+		for (j = 0;; j ++)
 		{
-			//skip
-			for (j = 0;; j ++)
-			{
-				fscanf(fp_spec,"%s",str);
-				if (strcmp("z):",str) == 0)
-					break;
-			}
-			n_x = (int *)malloc(num_nodes * sizeof(int));
-			n_y = (int *)malloc(num_nodes * sizeof(int));
+			fscanf(fp_spec,"%s",str);
+			if (strcmp("z):",str) == 0)
+				break;
+		}
+		n_x = (int *)malloc(num_nodes * sizeof(int));
+		n_y = (int *)malloc(num_nodes * sizeof(int));
 
-			//fprintf(fp_req, "LOCATIONS (x,y)\n");
-			for (j = 0; j < num_nodes; j ++)
-			{
-				fscanf(fp_spec, "%d %d %d %d", &t1, &t2, &n_x[j], &n_y[j]);
-				//printf("%d %d %d %d\n", t1, t2, n_x[j], n_y[j]);
-				double t = rand();
-				fprintf(fp_req, "%d %d %.2lf\n", n_x[j], n_y[j], t/(double)MY_RAND_MAX * (double)MAX_CPU);
-			}
-			//fprintf(fp_req,"\n");
+		//fprintf(fp_req, "LOCATIONS (x,y)\n");
+		for (j = 0; j < num_nodes; j ++)
+		{
+			fscanf(fp_spec, "%d %d %d %d", &t1, &t2, &n_x[j], &n_y[j]);
+			//printf("%d %d %d %d\n", t1, t2, n_x[j], n_y[j]);
+			//double t = rand();
+			//fprintf(fp_req, "%d %d %.2lf\n", n_x[j], n_y[j], t/(double)MY_RAND_MAX * (double)MAX_CPU);
 		}
 		// skip
 		for (j = 0;; j ++)
@@ -241,17 +246,43 @@ int main(int argc,char **argv)
 				break;
 		}
 		//fprintf(fp_req,"EDGES (from-node to-node)\n");
+		for (j = 0; j< MAX_NODE_NUM; j++)
+		{
+			degree[j] = 0;
+		}
+		from = (int *)malloc(num_edges * sizeof(int));
+		to = (int *)malloc(num_edges * sizeof(int));
 		for (j = 0; j < num_edges; j ++)
 		{
-			fscanf(fp_spec, "%d %d %*d %*d", &from, &to);
-			fprintf(fp_req, "%d %d %.2lf\n", from, to, rand()/(double)MY_RAND_MAX * (double)MAX_BW);
+			fscanf(fp_spec, "%d %d %*d %*d", &from[j], &to[j]);
+			//fprintf(fp_req, "%d %d %.2lf\n", from, to, rand()/(double)MY_RAND_MAX * (double)MAX_BW);
+			degree[from[j]] += 1;
+			degree[to[j]] += 1;
 		}
-		//fprintf(fp_req,"\n");
+		for(j = 0; j < num_nodes-1; j++)
+			fprintf(fp_req, "%d ", degree[j]);
+		fprintf(fp_req, "%d\n", degree[j]);
+		for(j = 0; j < num_nodes; j++)
+		{
+			double r1 = rand() / (double)MY_RAND_MAX;
+			double r2 = rand() / (double)MY_RAND_MAX;
+			fprintf(fp_req, "%d %d %.2lf %.2lf\n", n_x[j], n_y[j], r1*(MAX_CPU-MIN_CPU)+MIN_CPU, degree[j]*(r2*(MAX_TABLE-MIN_TABLE)+MIN_TABLE));
+		}
+		for(j = 0; j < num_edges; j++)
+		{
+			double r3 = rand() / (double)MY_RAND_MAX;
+			fprintf(fp_req, "%d %d %.2lf\n", from[j], to[j], r3*(MAX_BW-MIN_BW)+MIN_BW);
+		}
+		for (j = 0; j< num_nodes; j++)
+		{
+			printf("degree of node %d: %d\n",j,degree[j]);
+		}
+
 		fclose(fp_spec);
 		fclose(fp_req);
-		printf("the %d-th request generation finished!\n",i);
-
+		printf("the %d-th request generation finished!\n\n",i);
 	}
 
+	
     return 0;
 }
--- sub_top_gen_hhb.c
+++ sub_top_gen_hhb.c
@@ -7,61 +7,69 @@
 #define LEN_FILENAME 128
 #define LEN_CMD 128
 
-#define RAND_MAX 2147483647
+#define MY_RAND_MAX 2147483647
 
 #define MAX_CPU 100
+#define MIN_CPU 50
 #define MAX_BW 100
+#define MIN_BW 50
+#define MAX_TABLE 200
+#define MIN_TABLE 150
 
-double dis(int x1, int y1, int x2, int y2) {
-  return sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2));
+double dis(int x1, int y1, int x2, int y2)
+{
+  return sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
 }
 //计算两点之间的距离
-int main(int argc, char **argv) {
-  int xseed = (unsigned)time(NULL);  // Current Time
-  srand( xseed );
+int main(int argc, char **argv)
+{
+  int xseed = (unsigned)time(NULL); // Current Time
+  srand(xseed);
 
-  if (argc != 5) {
+  if (argc != 5)
+  {
     printf("mksub <n> <scale> <link_connectivity_rate> <dir_name>\n");
+    printf("n: number of nodes in graph\nscale: grid scale\n");
     exit(1);
   }
 
-  int n = atoi(argv[1]);  // number of nodes in graph
-  int scale = atoi(argv[2]);  // grid scale
+  int n = atoi(argv[1]);                 // number of nodes in graph
+  int scale = atoi(argv[2]);             // grid scale
   double link_conn_rate = atof(argv[3]); // link connectivity rate
 
-  FILE * fp;
-  FILE * reqfile;
+  FILE *fp;
+  FILE *reqfile;
   char filename[LEN_FILENAME], reqfilename[LEN_FILENAME];
   char cmd[LEN_CMD];
   int *n_x, *n_y, t1, t2;
 
   int num_nodes, num_edges, from, to;
-  int check_delete = system("rm -fr spec/itm*");//删除当前路径下spec目录下的以itm开头的文件
-   if(check_delete == -1)
-    {
-        printf("Error by hhb: fail to delete files\n");
-		exit(0);
-    }
-    check_delete = system("rm -fr alt/*.alt");//删除当前路径下alt目录下后缀名是.alt的所有文件
-    if(check_delete == -1)
-    {
-        printf("Error by hhb: fail to delete files\n");
-		exit(0);
-    }
+  int check_delete = system("rm -fr spec/itm*"); //删除当前路径下spec目录下的以itm开头的文件
+  if (check_delete == -1)
+  {
+    printf("Error by hhb: fail to delete files\n");
+    exit(0);
+  }
+  check_delete = system("rm -fr alt/*.alt"); //删除当前路径下alt目录下后缀名是.alt的所有文件
+  if (check_delete == -1)
+  {
+    printf("Error by hhb: fail to delete files\n");
+    exit(0);
+  }
 
-  sprintf(filename, "spec/itm-specsub");//在spec目录下新建一个itm-specsub文件
+  sprintf(filename, "spec/itm-specsub"); //在spec目录下新建一个itm-specsub文件
   fp = fopen(filename, "w");
 
-//  1: Waxman 1
-//  2: Waxman 2
-//  3: Pure random
-//  4: Doar-Leslie
-//  5: Exponential
-//  6: Locality
+  //  1: Waxman 1
+  //  2: Waxman 2
+  //  3: Pure random
+  //  4: Doar-Leslie
+  //  5: Exponential
+  //  6: Locality
 
-  fprintf(fp, "geo 1 %d\n",xseed); // generate one substrate graph
-  fprintf(fp, "%d %d 2 %.2lf 0.2\n", n, scale,link_conn_rate); // scale: 100; method: 2; alpha: 0.5; beta: 0.2; gamma: default
-  //fprintf(fp, "ts 1\n");
+  fprintf(fp, "geo 1 %d\n", xseed);                             // generate one substrate graph
+  fprintf(fp, "%d %d 3 %.2lf 0.2\n", n, scale, link_conn_rate); // scale: 100; method: 2; alpha: 0.5; beta: 0.2; gamma: default
+  //fp	rintf(fp, "ts 1\n");
   //fprintf(fp, "100 1 10\n");
   //fprintf(fp, "%d %d 3 %.2lf\n", n, scale,link_conn_rate);
   fclose(fp);
@@ -81,34 +89,35 @@ int main(int argc, char **argv) {
   sprintf(filename, "alt/sub.alt");
   fp = fopen(filename, "r");
 
-  	char req_folder[LEN_FILENAME];
+  char req_folder[LEN_FILENAME];
 
-	sprintf(req_folder,"./sub_topos/%s",argv[4]);
+  sprintf(req_folder, "./sub_topos/%s", argv[4]);
 
-	if(access(req_folder,2) != 0)//检查写权限，如果文件存在access值为0，否则为-1
-    {
-        mkdir(req_folder,0777);//权限为777的意思是权限全开，是最高权限
-    }
-	else
-    {
-        char common_d[LEN_FILENAME];
-        sprintf(common_d,"rm -fr %s/*",req_folder);
-        system(common_d);
-    }
-  sprintf(reqfilename, "%s/sub_%s_%s.txt",req_folder,argv[1],argv[2]);
+  if (access(req_folder, 2) != 0) //检查写权限，如果文件存在access值为0，否则为-1
+  {
+    mkdir(req_folder, 0777); //权限为777的意思是权限全开，是最高权限
+  }
+  else
+  {
+    char common_d[LEN_FILENAME];
+    sprintf(common_d, "rm -fr %s/*", req_folder);
+    system(common_d);
+  }
+  sprintf(reqfilename, "%s/sub_%s_%s.txt", req_folder, argv[1], argv[2]);
   reqfile = fopen(reqfilename, "w");
 
   // remove preamble
-  for (j = 0; j < 10; j ++)
+  for (j = 0; j < 10; j++)
     fscanf(fp, "%s", str);
 
   fscanf(fp, "%d %d %*d %*d", &num_nodes, &num_edges);
   num_edges /= 2;
-  
-  //fprintf(reqfile,"SIZES (number-of-nodes number-of-edges)\n%d %d\n\n",num_nodes,num_edges);
-  fprintf(reqfile, "%d %d\n", num_nodes, num_edges);
 
-  for (j = 0; j < 11; j ++) {
+  //fprintf(reqfile,"SIZES (number-of-nodes number-of-edges)\n%d %d\n\n",num_nodes,num_edges);
+  fprintf(reqfile, "%d %d %d\n", num_nodes, num_edges, scale);
+
+  for (j = 0; j < 11; j++)
+  {
     fscanf(fp, "%s", str);
     printf("%s\n", str);
   }
@@ -118,23 +127,29 @@ int main(int argc, char **argv) {
 
   // generate CPU resources for the nodes
   //fprintf(reqfile, "LOCATIONS (x,y)\n");
-  for (j = 0; j < num_nodes; j ++) {
+  for (j = 0; j < num_nodes; j++)
+  {
     fscanf(fp, "%d %d %d %d", &t1, &t2, &n_x[j], &n_y[j]);
-    printf("%d %d %d %d %.2lf\n", t1, t2, n_x[j], n_y[j], (rand()/(double)RAND_MAX +1)*(double)MAX_CPU*0.5);
-    fprintf(reqfile, "%d %d %.2lf\n", n_x[j], n_y[j], (rand()/(double)RAND_MAX +1)*(double)MAX_CPU*0.5);
+    double r1 = rand() / (double)MY_RAND_MAX;
+    double r2 = rand() / (double)MY_RAND_MAX;
+    printf("%d %d %d %d %.2lf %.2lf\n", t1, t2, n_x[j], n_y[j], r1 * (MAX_CPU - MIN_CPU) + MIN_CPU, r2 * (MAX_TABLE - MIN_TABLE) + MIN_TABLE);
+    fprintf(reqfile, "%d %d %.2lf %.2lf\n", n_x[j], n_y[j], r1 * (MAX_CPU - MIN_CPU) + MIN_CPU, r2 * (MAX_TABLE - MIN_TABLE) + MIN_TABLE);
   }
 
-  for (j = 0; j < 6; j ++) {
+  for (j = 0; j < 6; j++)
+  {
     fscanf(fp, "%s", str);
     printf("%s\n", str);
   }
 
   //fprintf(reqfile,"EDGES (from-node to-node)\n");
   // generate bandwidth for the links
-  for (j = 0; j < num_edges; j ++) {
+  for (j = 0; j < num_edges; j++)
+  {
     fscanf(fp, "%d %d %*d %*d", &from, &to);
-	printf("%d %d %.2lf %.2lf\n", from, to, (rand()/(double)RAND_MAX +1) * (double)MAX_BW * 0.5, dis(n_x[from],n_y[from],n_x[to],n_y[to]));
-    fprintf(reqfile, "%d %d %.2lf %.2lf\n", from, to, (rand()/(double)RAND_MAX +1) * (double)MAX_BW * 0.5, dis(n_x[from],n_y[from],n_x[to],n_y[to]));
+    double r3 = rand() / (double)MY_RAND_MAX;
+    printf("%d %d %.2lf %.2lf\n", from, to, r3 * (MAX_BW - MIN_BW) + MIN_BW, dis(n_x[from], n_y[from], n_x[to], n_y[to]));
+    fprintf(reqfile, "%d %d %.2lf %.2lf\n", from, to, r3 * (MAX_BW - MIN_BW) + MIN_BW, dis(n_x[from], n_y[from], n_x[to], n_y[to]));
   }
 
   free(n_x);
@@ -142,18 +157,5 @@ int main(int argc, char **argv) {
 
   fclose(fp);
   fclose(reqfile);
-  //int check_delete = system("rm -fr spec/itm*");
-   if(check_delete == -1)
-    {
-        printf("Error by hhb: fail to delete files\n");
-		exit(0);
-    }
-    check_delete = system("rm -fr alt/*.alt");
-    if(check_delete == -1)
-    {
-        printf("Error by hhb: fail to delete files\n");
-		exit(0);
-    }
   return 0;
 }
-
